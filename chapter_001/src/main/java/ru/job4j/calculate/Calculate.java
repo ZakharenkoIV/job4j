7c3d75f2c261f455ab8ab06f12c0bd2e75b581bc
ZakharenkoIV <ZakharenkoIV@yandex.ru>
2018-12-16 19:52:43 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ *Class Calculate решение задачи части 001 урок 1.3
+ *@author Ilya Zakharenko (ZakharenkoIV@yandex.ru)
+ *@since 16.12.2018
+ */
 public class Calculate {
+	
+	/**
+	 *Стартовый метод программы
+	 *выводит в консоль текст "Hello world" 
+	 *@param args - args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Zakharenko (ZakharenkoIV@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
