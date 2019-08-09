f1fce3f4d2adc59d7bed1b480a174748dc2dad18
ZakharenkoIV <ZakharenkoIV@yandex.ru>
2019-08-09 17:07:49 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/main/java/ru/job4j/trainee/part002/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/trainee/part002/tracker/StartUI.java
@@ -13,7 +13,6 @@ public class StartUI {
     private static final String EXIT = "6";
     private final Tracker tracker;
     private final Input input;
-    public static String idOfLastItemAdded;
 
     public StartUI(Input input, Tracker tracker) {
         this.input = input;
@@ -60,8 +59,7 @@ public class StartUI {
         String desc = this.input.ask("Введите описание заявки : ");
         Item item = new Item(name, desc);
         item = this.tracker.add(item);
-        idOfLastItemAdded = item.getId();
-        System.out.println("------------ ID новой заявки : " + idOfLastItemAdded + "------------");
+        System.out.println("------------ ID новой заявки : " + item.getId() + "------------");
     }
 
     private void showAllItems() {
--- chapter_002/src/test/java/ru/job4j/trainee/part002/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/trainee/part002/tracker/StartUITest.java
@@ -6,15 +6,21 @@ import org.junit.After;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
-    private static PrintStream stdout = System.out;
-    private static ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     Tracker tracker = new Tracker();
-    Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+    Item item1 = tracker.add(new Item("test name1", "desc1"));
+    Item item2 = tracker.add(new Item("test name2", "desc2"));
+    Item item3 = tracker.add(new Item("test name2", "desc3"));
+    Input input1 = new StubInput(new String[]{"1", "6"});
+    Input input2 = new StubInput(new String[]{"4", item2.getId(), "6"});
+    Input input3 = new StubInput(new String[]{"5", item3.getName(), "6"});
     String menu = "Меню"
             + "\r\n" + "(0) Добавить новую заявку"
             + "\r\n" + "(1) Показать все заявки"
@@ -35,18 +41,53 @@ public class StartUITest {
     }
 
     @Test
-    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        new StartUI(input, tracker).init();
+    public void whenUserShowAllItems() {
+        new StartUI(input1, tracker).init();
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                         .append(menu)
-//                        .append("\r\n" + "Ввеедите пункт меню : ")
-                        .append("\r\n" + "------------ Добавление новой заявки ------------")
-//                        .append("\r\n" + "Введите имя заявки : ")
-//                        .append("\r\n" + "Введите описание заявки : ")
-                        .append("\r\n" + "------------ ID новой заявки : " + StartUI.idOfLastItemAdded + "------------")
+                        .append("\r\n" + "Ввеедите пункт меню : ")
+                        .append("\r\n" + "------------ Все заявки ------------")
+                        .append("\r\n" + Arrays.toString(tracker.findAll()))
                         .append("\r\n")
                         .append(menu)
-//                        .append("\r\n" + "Ввеедите пункт меню : ")
+                        .append("\r\n" + "Ввеедите пункт меню : ")
+                        .append(System.lineSeparator())
+                        .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenUserFindItemById() {
+        new StartUI(input2, tracker).init();
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                        .append(menu)
+                        .append("\r\n" + "Ввеедите пункт меню : ")
+                        .append("\r\n" + "------------ Поиск заявки по ID ------------")
+                        .append("\r\n" + "Введите ID заявки : ")
+                        .append("\r\n" + "------------ " + item2 + "------------")
+                        .append("\r\n")
+                        .append(menu)
+                        .append("\r\n" + "Ввеедите пункт меню : ")
+                        .append(System.lineSeparator())
+                        .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenUserFindItemsByName() {
+        new StartUI(input3, tracker).init();
+        Item[] items = this.tracker.findByName(item3.getName());
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                        .append(menu)
+                        .append("\r\n" + "Ввеедите пункт меню : ")
+                        .append("\r\n" + "------------ Поиск заявок по имени ------------")
+                        .append("\r\n" + "Введите имя заявки : ")
+                        .append("\r\n" + Arrays.toString(items))
+                        .append("\r\n")
+                        .append(menu)
+                        .append("\r\n" + "Ввеедите пункт меню : ")
                         .append(System.lineSeparator())
                         .toString()
                 )
