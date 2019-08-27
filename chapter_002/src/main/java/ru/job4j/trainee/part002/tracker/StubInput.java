f88c238b67f312f0135ce95325cf753a5a5ae7bc
ZakharenkoIV <ZakharenkoIV@yandex.ru>
2019-08-27 17:46:11 +0300

    1. Обеспечить бесперебойную работу приложения Tracker. [#789]

--- chapter_002/src/main/java/ru/job4j/trainee/part002/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/trainee/part002/tracker/StubInput.java
@@ -1,6 +1,6 @@
 package ru.job4j.trainee.part002.tracker;
 
-public class StubInput implements Input {
+public class StubInput extends ValidateInput {
     private final String[] answers;
     private int position = 0;
 
@@ -11,10 +11,5 @@ public class StubInput implements Input {
     public String ask(String question) {
         System.out.println(question);
         return answers[position++];
-    }
-
-    @Override
-    public int ask(String question, int[] range) {
-        throw new UnsupportedOperationException("Unsupported operation");
     }
 }
