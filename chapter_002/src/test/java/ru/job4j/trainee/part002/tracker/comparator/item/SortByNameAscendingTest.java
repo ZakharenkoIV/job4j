a9944f7330b50c609d1e9ad27dd3ae2e5fdadd0c
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-04-28 14:20:55 +0300

    0. Сортировка [#10096]

--- chapter_002/src/test/java/ru/job4j/trainee/part002/tracker/comparator/item/SortByNameAscendingTest.java
+++ chapter_002/src/test/java/ru/job4j/trainee/part002/tracker/comparator/item/SortByNameAscendingTest.java
@@ -5,7 +5,6 @@ import ru.job4j.trainee.part002.tracker.Item;
 import ru.job4j.trainee.part002.tracker.comparators.item.SortByNameAscending;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -15,16 +14,12 @@ public class SortByNameAscendingTest {
 
     @Test
     public void sortByNameAscendingTest() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("d"));
-        items.add(new Item("c"));
-        items.add(new Item("a"));
-        items.add(new Item("b"));
-        items.sort(new SortByNameAscending());
-        String actual = "[\n" + "Имя заявки: a\n"
-                     + ", \n" + "Имя заявки: b\n"
-                     + ", \n" + "Имя заявки: c\n"
-                     + ", \n" + "Имя заявки: d\n" + "]";
-        assertThat((items.toString()), is(actual));
+        Item itemA = new Item("A");
+        Item itemB = new Item("B");
+        Item itemC = new Item("C");
+        List<Item> input = new ArrayList<>(List.of(itemC, itemB, itemA));
+        input.sort(new SortByNameAscending());
+        List<Item> expected = List.of(itemA, itemB, itemC);
+        assertThat((input), is(expected));
     }
 }
--- chapter_002/src/test/java/ru/job4j/trainee/part002/tracker/comparator/item/SortByNameDescendingTest.java
+++ chapter_002/src/test/java/ru/job4j/trainee/part002/tracker/comparator/item/SortByNameDescendingTest.java
@@ -5,7 +5,6 @@ import ru.job4j.trainee.part002.tracker.Item;
 import ru.job4j.trainee.part002.tracker.comparators.item.SortByNameDescending;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -14,17 +13,13 @@ import static org.junit.Assert.assertThat;
 public class SortByNameDescendingTest {
 
     @Test
-    public void sortByNameAscendingTest() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("d"));
-        items.add(new Item("c"));
-        items.add(new Item("a"));
-        items.add(new Item("b"));
-        items.sort(new SortByNameDescending());
-        String actual = "[\n" + "Имя заявки: d\n"
-                     + ", \n" + "Имя заявки: c\n"
-                     + ", \n" + "Имя заявки: b\n"
-                     + ", \n" + "Имя заявки: a\n" + "]";
-        assertThat((items.toString()), is(actual));
+    public void sortByNameDescendingTest() {
+        Item itemA = new Item("A");
+        Item itemB = new Item("B");
+        Item itemC = new Item("C");
+        List<Item> input = new ArrayList<>(List.of(itemA, itemB, itemC));
+        input.sort(new SortByNameDescending());
+        List<Item> expected = List.of(itemC, itemB, itemA);
+        assertThat((input), is(expected));
     }
 }
